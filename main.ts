2aa5d3fd2622f8067d6a90cac86b4d25f42af588
fishiversity <41026299+fishiversity@users.noreply.github.com>
2022-12-19 14:26:21 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,18 @@
+radio.setGroup(0)
+basic.forever(function () {
+    if (input.isGesture(Gesture.TiltLeft)) {
+        radio.sendNumber(4)
+    } else if (input.isGesture(Gesture.TiltRight)) {
+        radio.sendNumber(3)
+    } else if (input.isGesture(Gesture.LogoDown)) {
+        radio.sendNumber(2)
+    } else if (input.isGesture(Gesture.LogoUp)) {
+        radio.sendNumber(1)
+    } else if (input.buttonIsPressed(Button.A)) {
+        radio.sendNumber(5)
+    } else if (input.buttonIsPressed(Button.B)) {
+        radio.sendNumber(6)
+    } else {
+        radio.sendNumber(0)
+    }
+})
